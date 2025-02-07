e45d6d847031ff88b649ec9c804237f2073fa531
nut <nut999anan@hotmail.com>
2025-02-07 17:17:57 +0700

    mobile nav done

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { ITEMS, sidebarLinks } from "../../constants";
+import { sidebarLinks } from "../../constants";
 import { usePathname } from "next/navigation";
 import { cn } from "../../lib/utils";
 
