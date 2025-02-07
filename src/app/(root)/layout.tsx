573cb4607bc9861fea1cb4773e511f584a30b083
anan <nut999anan@hotmail.com>
2025-02-07 16:24:01 +0700

    sidebar 50%

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,14 +1,16 @@
-import { ThemeToggleButton } from "../../components/theme/ThemeToggleButton";
-
+import SideBar from "@/components/SideBar";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {
+    firstName: "Anan",
+    lastName: "MMP",
+  };
   return (
-    <main suppressHydrationWarning>
-      SIDEBAR
-      <ThemeToggleButton />
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
       {children}
     </main>
   );
